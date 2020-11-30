7490bc4da7c5c0f6753fb912f12aceb00c9ed8cb
ogneyar <ogneyar-ne@ya.ru>
2020-11-30 15:27:43 +0300

    q

--- server.js
+++ server.js
@@ -1,29 +1,6 @@
-require('dotenv').config();
-
 var express = require('express');
 var	port = process.env.PORT || 8000;
-var	app = express();
 	
-var http = require('http');
-var	reload = require('reload');
-
-app.use(express.static('dist/Angular'));
-
-app.get('/', function(req, res){
-    res.render('index.html');
-});
-
-app.get('/crisis-list', function(req, res){
-    res.render('controllers/crisis.html');
-});
-
-app.get('/heroes-list', function(req, res){
-    res.render('controllers/heroes.html');
-});
-
-var server = http.createServer(app);
-reload(app).then(function () {	
-	server.listen(port, () => console.log(`Starting my server on NodeJS: Listening on ${ port }`));
-}).catch(function (err) {
-	console.error('Reload could not start, could not start server/sample app', err);
-});
+express().use(express.static('dist'))
+    .get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
+    .listen(port, () => console.log(`Starting server. Listening on ${ port }`));
