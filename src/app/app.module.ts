a0f0cfc9e0a87b40978c3b10d23dbf1c24e8885b
ogneyar <ogneyar-ne@ya.ru>
2021-01-19 16:52:42 +0300

    tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { HouseKipreyComponent } from './house-kiprey/house-kiprey.component';
+import { BaseComponent } from './layouts/base/base.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HouseKipreyComponent } from './house-kiprey/house-kiprey.component';
     SelectorComponent,
     MomentPipe,
     CalculatorComponent,
-    HouseKipreyComponent
+    HouseKipreyComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
